702a79f71c3c59288a29e170f56b39099c192ccf
anhtumai <tu.mai@outlook.com>
2021-07-12 22:14:41 +0300

    Ex 4.5, Add favorite blog helper method

--- src/utils/list_helper.ts
+++ src/utils/list_helper.ts
@@ -1,7 +1,34 @@
-const dummy = (blogs: any[]) => {
+export interface Blog {
+    _id: string
+    title: string
+    author: string
+    url: string
+    likes: number
+    __v: number
+}
+
+function dummy(blogs: Blog[]) {
     return 1
+}
+
+function totalLikes(blogs: Blog[]) {
+    return blogs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.likes,
+        0
+    )
+}
+
+function favoriteBlog(blogs: Blog[]) {
+    if (blogs.length === 0) return null
+    return blogs.reduce(
+        (prevBlog, currentBlog) =>
+            prevBlog.likes < currentBlog.likes ? currentBlog : prevBlog,
+        blogs[0]
+    )
 }
 
 export default {
     dummy,
+    totalLikes,
+    favoriteBlog,
 }
