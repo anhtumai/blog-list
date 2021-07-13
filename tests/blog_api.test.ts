a70244bab888e74dae9ef3d1098512142128f29c
anhtumai <tu.mai@outlook.com>
2021-07-13 15:31:00 +0300

    Ex 4.9, add all test cases of GET request in 1 single describe block

--- tests/blog_api.test.ts
+++ tests/blog_api.test.ts
@@ -15,36 +15,38 @@ beforeEach(async () => {
     }
 })
 
-test('notes are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+describe('Test get request on /api/blogs', () => {
+    test('notes are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(initialBlogs.length)
-})
+        expect(response.body).toHaveLength(initialBlogs.length)
+    })
 
-test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
 
-    console.log(response.body)
-    const contents = response.body.map((blog) => blog.title)
-    expect(contents).toContain('Type wars')
-})
+        console.log(response.body)
+        const contents = response.body.map((blog) => blog.title)
+        expect(contents).toContain('Type wars')
+    })
 
-test('identifier property of blog post is named id', async () => {
-    const response = await api.get('/api/blogs')
+    test('identifier property of blog post is named id', async () => {
+        const response = await api.get('/api/blogs')
 
-    const firstBlog = response.body[0]
+        const firstBlog = response.body[0]
 
-    expect(firstBlog._id).toBeUndefined()
-    expect(firstBlog.__v).toBeUndefined()
-    expect(firstBlog.id).toBeDefined()
-    expect(typeof firstBlog.id).toBe('string')
+        expect(firstBlog._id).toBeUndefined()
+        expect(firstBlog.__v).toBeUndefined()
+        expect(firstBlog.id).toBeDefined()
+        expect(typeof firstBlog.id).toBe('string')
+    })
 })
 
 afterAll(() => {
