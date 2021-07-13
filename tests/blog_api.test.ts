0682bcae4cc9f52e4d41c7c8128c33867a80c860
anhtumai <tu.mai@outlook.com>
2021-07-13 22:34:45 +0300

    Ex 4.12, add POST request test into 1 describe block

--- tests/blog_api.test.ts
+++ tests/blog_api.test.ts
@@ -48,55 +48,58 @@ describe('Test get request on /api/blogs', () => {
     })
 })
 
-test('if length of blogs increase by 1 when making a post request', async () => {
-    const newBlog = {
-        title: 'FP vs. OO',
-        author: 'Robert C. Martin',
-        url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
-        likes: 8,
-    }
+describe('Test POST request on /api/blogs', () => {
+    test('if length of blogs increase by 1 when making a post request', async () => {
+        const newBlog = {
+            title: 'FP vs. OO',
+            author: 'Robert C. Martin',
+            url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
+            likes: 8,
+        }
 
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const currentBlogs = await BlogModel.find({})
-    expect(currentBlogs).toHaveLength(initialBlogs.length + 1)
+        const currentBlogs = await BlogModel.find({})
+        expect(currentBlogs).toHaveLength(initialBlogs.length + 1)
+    })
+
+    test('if likes property is missing when making a post request, it will default to 0', async () => {
+        const newBlog = {
+            title: 'FP vs. OO',
+            author: 'Robert C. Martin',
+            url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
+        }
+
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.likes).toEqual(0)
+        const newBlogsInDB = await BlogModel.find({ title: newBlog.title })
+        expect((newBlogsInDB[0] as any).likes).toEqual(0)
+    })
+
+    test('if url or title property is missing when making post request, response with 400', async () => {
+        const missingBlog1 = {
+            title: 'FP vs. OO',
+            author: 'Robert C. Martin',
+        }
+        const missingBlog2 = {
+            author: 'Robert C. Martin',
+            url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
+        }
+
+        await api.post('/api/blogs').send(missingBlog1).expect(400)
+        await api.post('/api/blogs').send(missingBlog2).expect(400)
+    })
 })
 
-test('if likes property is missing when making a post request, it will default to 0', async () => {
-    const newBlog = {
-        title: 'FP vs. OO',
-        author: 'Robert C. Martin',
-        url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
-    }
-
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    expect(response.body.likes).toEqual(0)
-    const newBlogsInDB = await BlogModel.find({ title: newBlog.title })
-    expect((newBlogsInDB[0] as any).likes).toEqual(0)
-})
-
-test('if url or title property is missing when making post request, response with 400', async () => {
-    const missingBlog1 = {
-        title: 'FP vs. OO',
-        author: 'Robert C. Martin',
-    }
-    const missingBlog2 = {
-        author: 'Robert C. Martin',
-        url: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
-    }
-
-    await api.post('/api/blogs').send(missingBlog1).expect(400)
-    await api.post('/api/blogs').send(missingBlog2).expect(400)
-})
 afterAll(() => {
     mongoose.connection.close()
 })
