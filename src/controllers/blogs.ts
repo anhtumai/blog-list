72b35e60f348aa019a9b212db654f31d706eb6a3
anhtumai <tu.mai@outlook.com>
2021-07-14 04:12:58 +0300

    Ex 4.14, add PUT request

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -40,4 +40,35 @@ blogsRouter.delete('/:id', async (req, res) => {
     }
 })
 
+blogsRouter.put('/:id', async (req, res) => {
+    const { url, author, title, likes } = req.body
+
+    if (
+        url === undefined ||
+        author === undefined ||
+        title === undefined ||
+        likes === undefined
+    ) {
+        return res.status(400).json({ error: 'Info is missing' })
+    }
+
+    const { id } = req.params
+
+    try {
+        const updatedBlog = await BlogModel.findOneAndUpdate(
+            { _id: id },
+            { url, author, title, likes },
+            { new: true }
+        )
+        if (updatedBlog) {
+            return res.status(201).json(updatedBlog)
+        } else {
+            return res.status(404).end()
+        }
+    } catch (err) {
+        console.log(err.message)
+        return res.status(400).end()
+    }
+})
+
 export default blogsRouter
